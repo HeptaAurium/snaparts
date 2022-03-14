28ea119d454038f0e44e14424539b71f7256629e
Ngugi Kiarie <ngugithe4th@gmail.com>
2022-03-14 18:57:10 +0300

    Navigation and showcase

--- index.php
+++ index.php
@@ -6,14 +6,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Snap@rts</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 
 <body>
-    <main>
-        <header></header>
-        <footer></footer>
+    <main class="main">
+        <header class="header">
+            <div class="logo">
+                Snap@rts
+            </div>
+            <ul class="top-nav">
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">Photo Retouch</a>
+                </li>
+                <li>
+                    <a href="">Shop</a>
+                </li>
+                <li>
+                    <a href="">Gallery</a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
+                </li>
+            </ul>
+        </header>
+        <div class="showcase"></div>
     </main>
+    <footer></footer>
 </body>
 
 </html>
